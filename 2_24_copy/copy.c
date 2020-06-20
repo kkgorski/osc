1be9178921687675bdb50209c7ea1cb56b55bf2d
kkgorski <jgorski63@gmail.com>
2020-06-20 19:35:24 +0200

    [2.24] First commit with c ut structure

---
+++ 2_24_copy/copy.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+   printf("This is my copy program!");
+   return 0;
+}
---
+++ 2_24_copy/testCopy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "minunit.h"
+
+int tests_run = 0;
+
+static char * test_copy() {
+    mu_assert("error, copy doesn't work", 0);
+    return 0;
+}
+
+static char * all_tests() {
+    mu_run_test(test_copy);
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    char *result = all_tests();
+    if (result != 0) {
+        printf("%s\n", result);
+    }
+    else {
+        printf("ALL TESTS PASSED\n");
+    }
+    printf("Tests run: %d\n", tests_run);
+
+    return result != 0;
+}
